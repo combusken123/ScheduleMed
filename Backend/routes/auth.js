b731d98dc69dcdd5815ef95578fd96aa6c659e8f
combusken123 <combuskenisthebest@gmail.com>
2022-03-14 08:27:02 +1300

    Second Commit
    Added the final routed auth
    Need to add specified auth for doctor vs patient

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require('../model/User');
+const jwt = require('jsonwebtoken');
 const { registerValidation, loginValidation } = require('../validation');
 const bcrypt = require('bcryptjs');
 
@@ -11,24 +12,44 @@ router.post('/register', async(req, res) => {
 
     // User exists?
     const emailExist = await User.findOne({ email: req.body.email });
-    if (emailExist) return res.statusCode(400).send('Email already exists');
+    if (emailExist) return res.status(400).send('Email already exists');
 
     //Hash the password
     const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
     const user = new User({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword,
     });
     try {
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({ user: user.id });
     } catch(err) {
         res.status(400).send(err);
         console.log(err);
     }
 });
+
+// Login
+router.post('/login', async (req, res) => {
+    // Validate Data
+    const { error } = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // User exists?
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Email or Password is incorrect');
+
+    // Correct password?
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if (!validPass) return res.status(400).send('Email or Password is incorrect');
+
+    // Create and assign a token
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    res.header('auth-token', token);
+})
 
 module.exports = router;
--- Backend/validation.js
+++ Backend/validation.js
@@ -1,21 +1,21 @@
 const Joi = require('@hapi/joi');
 
 const registerValidation = data => {
-    const schema = {
+    const schema = Joi.object({
         firstName: Joi.string().min(2).required(),
         lastName: Joi.string().min(2).required(),
         email: Joi.string().min(6).required(),
         password: Joi.string().min(6).required()
-    };
-    return Joi.validate(data, schema);
+    });
+    return schema.validate(data);
 };
 
 const loginValidation = data => {
-    const schema = {
+    const schema = Joi.object({
         email: Joi.string().min(6).required(),
         password: Joi.string().min(6).required()
-    };
-    return Joi.validate(data, schema);
+    });
+    return schema.validate(data);
 };
 
 module.exports.registerValidation = registerValidation;
